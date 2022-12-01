6874dff62ca3c2b2139839cdbfb5e8c22d63bbf1
Francisco Emmanuel Dumas <91019174+Franz0204@users.noreply.github.com>
2022-12-02 01:18:56 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,12 +9,17 @@
 
 
 void Dice(int *roll);
+void Side(int Player, char *Side);
+void Board(int table[][MAXCOLS], int control[][MAXCOLS], int captured[], char player[]);
 
 int main(){
 
 int i, j, roll1 = 0, roll2 = 0;
 int table[MAXROWS][MAXCOLS] = {{5,0,0,0,3,0,5,0,0,0,0,2}, {5,0,0,0,3,0,5,0,0,0,0,2}};
-int captured[2] = {5,0};
+int control[MAXROWS][MAXCOLS] = {{1,0,0,0,-1,0,-1,0,0,0,0,1}, {-1,0,0,0,1,0,1,0,0,0,0,-1}}; // 1 = Player 1 / -1 = Player 2 
+int captured[2] = {0,0};
+char player[100]; 
+
 srand(time(NULL));
 
 printf("CCPROG Machine Project | Term 1 AY 2022-2023\n\n");
@@ -25,31 +30,7 @@ printf("\n|-[Backgammon]-|\n");
 
 
 //Table
-printf("---------------------------------------------\n");
-for (i = 0; i < MAXROWS; i++){
-
-    for (j = 0; j < MAXCOLS; j++){
-        printf("|");
-        printf("[%d]", table[i][j]);
-        if (j == 5) printf("|-|");
-    }
-
- printf("|");    
- printf("\n");
-}
-printf("---------------------------------------------\n");
-printf("//////////////////////////////////////////// \n");
-printf("---------------------------------------------");
-printf("\n|-[Captured]-|\n");
-printf("Player 1:[");
-if (captured [0] == 0) printf("FREE");
-for (i = 0; i < captured [0]; i++) printf(" / ");
-printf("]\n");
-printf("Player 2:[");
-if (captured [1] == 0) printf("FREE");
-for (i = 0; i < captured [1]; i++) printf(" / ");
-printf("]\n");
-printf("---------------------------------------------\n");
+Board(table, control, captured, player);
 
 printf("\nPress ENTER to Roll");
 scanf("%*[^\n]");
@@ -63,6 +44,62 @@ printf("Dice 2: [%d]", roll2);
 
     return 0;
 }
+
+void Board(int table[][MAXCOLS], int control[][MAXCOLS], int captured[], char player[])
+{
+    int i, j;
+
+    printf("----------------------------------------------------\n");
+    for (i = 0; i < MAXROWS/2; i++){
+
+        for (j = 0; j < MAXCOLS; j++){
+            Side(control[i][j], player);
+            printf("|");
+            printf("[%s]", player);
+            if (j == 5) printf("|_|");
+            }
+
+        printf("|");
+    }
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    for (i = 0; i < MAXROWS; i++){
+
+        for (j = 0; j < MAXCOLS; j++){
+            printf("|");
+            printf("[%d]", table[i][j]);
+            if (j == 5) printf("|-|");
+            }
+
+        printf("|");    
+        printf("\n");
+    }
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    for (i = 1; i < MAXROWS; i++){
+
+        for (j = 0; j < MAXCOLS; j++){
+            Side(control[i][j], player);
+            printf("|");
+            printf("[%s]", player);
+            if (j == 5) printf("|_|");
+            }
+
+        printf("|");
+    }
+    printf("\n");
+    printf("----------------------------------------------------");
+    printf("\n|-[Captured]-|\n");
+    printf("Player 1:[");
+    if (captured [0] == 0) printf("FREE");
+    for (i = 0; i < captured [0]; i++) printf(" / ");
+    printf("]\n");
+    printf("Player 2:[");
+    if (captured [1] == 0) printf("FREE");
+    for (i = 0; i < captured [1]; i++) printf(" / ");
+    printf("]\n");
+    printf("---------------------------------------------\n");
+  
+}
+
 
 void Dice(int *roll)
 {
@@ -96,6 +133,27 @@ void Dice(int *roll)
 
     case 5:
     *roll = 6;
+        break;
+    }
+}
+
+void Side(int Player, char *Side)
+{
+    switch (Player)
+    {
+    case -1:
+        strcpy(Side, "B");
+        break;
+    
+    case 0:
+        strcpy(Side, "-");
+        break;
+
+    case 1:
+        strcpy(Side, "W");
+        break;
+    
+    default:
         break;
     }
 }
